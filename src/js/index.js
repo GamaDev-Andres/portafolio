10aa8125bf4ddb57024c10b6e3c5062a4d01f3e4
Andres Gama <gama_pipe@hotmail.com>
2021-06-08 23:39:21 -0500

    configuracion webpack y pruebas

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import "../styles/style.scss";
+
+const arreglo = [1, 2, 3, 4, 5, 6];
+const arreglo2 = [7, 8, 9, 10];
+let arregloNuevo = [...arreglo, ...arreglo2];
+
+console.log(arregloNuevo);
+const miFuncion = (...arr) => arr.map((obj) => obj * 2);
+let resultado = miFuncion(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log(resultado);
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,20 +1,20 @@
 const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CssminimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserWebpackPlugin = require("terser-webpack-plugin");
+
 module.exports = {
     entry: "./src/js/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name][hash].js",
     },
+    mode: "development",
+    devtool: "source-map",
     resolve: {
         extensions: [".js"],
         // alias
     },
-    mode: "production",
     module: {
         rules: [
             {
@@ -49,9 +49,6 @@ module.exports = {
             {
                 test: /(png|jpe?g|svg|webp|woff|woff2)$/i,
                 type: "asset/resource",
-                use: {
-                    loader: "image-webpack-loader",
-                },
                 generator: {
                     filename: "./img/[hash][ext][query]",
                 },
@@ -60,17 +57,11 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            inject: "body",
             template: "./public/index.html",
             filename: "./index.html",
         }),
         new MiniCssExtractPlugin({
             filename: "assets / [name][contenthash].css",
         }),
-        new CleanWebpackPlugin(),
     ],
-    optimization: {
-        minimize: true,
-        minimizer: [new CssminimizerWebpackPlugin(), new TerserWebpackPlugin()],
-    },
 };
