393b1101b2e6b1c04d7b1cddd3136acbb14bec95
Andres Gama <gama_pipe@hotmail.com>
2021-10-23 00:18:30 -0500

    alistando para despliegue

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,21 @@ import { typed, tecnologias } from "./typed.js";
 
 particlesJS(obj);
 document.addEventListener("DOMContentLoaded", interaccionCarrusel);
+const menuHamburguesa = document.querySelector(".menu-hamburguesa");
+const btnCerrar = document.querySelector(".btn-cerrar");
+const enlacesMenu = document.querySelector(".enlaces-menu");
+// funcionalidad menu hamburguesas
+
+const mq = matchMedia("(min-width:768px)");
+mq.addEventListener("change", () => {
+  const cambio = mq.matches;
+  if (cambio) {
+    enlacesMenu.classList.remove("active");
+  }
+});
+menuHamburguesa.addEventListener("click", () => {
+  enlacesMenu.classList.add("active");
+});
+btnCerrar.addEventListener("click", () => {
+  enlacesMenu.classList.remove("active");
+});
