c0c4dcc35b7656db55c060d5a52b33c25c02ab8f
Andres Gama <gama_pipe@hotmail.com>
2022-01-25 12:49:51 -0500

    modificando tecs y agregando redes

--- src/js/carrusel.js
+++ src/js/carrusel.js
@@ -1,34 +1,33 @@
 export const interaccionCarrusel = () => {
-    new Glider(document.querySelector(".lista-carrusel"), {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        draggable: true,
-        dots: ".indicadores-carrusel",
-        arrows: {
-            prev: ".carrusel-anterior",
-            next: ".carrusel-siguiente",
+  new Glider(document.querySelector(".lista-carrusel"), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: ".indicadores-carrusel",
+    arrows: {
+      prev: ".carrusel-anterior",
+      next: ".carrusel-siguiente",
+    },
+    responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 775,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          duration: 0.75,
         },
-        responsive: [
-            {
-                // screens greater than >= 775px
-                breakpoint: 775,
-                settings: {
-                    // Set to `auto` and provide item width to adjust to viewport
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    duration: 0.75,
-                },
-            },
-            {
-                // screens greater than >= 1024px
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    duration: 0.75,
-                },
-            },
-        ],
-    });
-    console.log("hola desde otro archivo");
+      },
+      {
+        // screens greater than >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          duration: 0.75,
+        },
+      },
+    ],
+  });
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import "../styles/style.scss";
 import { interaccionCarrusel } from "./carrusel";
 import { obj } from "./particle.js";
-import { typed, tecnologias } from "./typed.js";
+import "./typed.js";
 
 particlesJS(obj);
 document.addEventListener("DOMContentLoaded", interaccionCarrusel);
